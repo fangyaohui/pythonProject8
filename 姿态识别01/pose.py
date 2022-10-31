95f10da81874a0d880e297052a52d5abff610b55
fangyaohui <2944163240@qq.com>
2022-10-31 15:50:19 +0800

    姿态识别

---
+++ 姿态识别01/pose.py
@@ -0,0 +1,27 @@
+import time
+import cv2
+import  mediapipe as mp
+
+mpPose = mp.solutions.pose
+pose = mpPose.Pose()
+mpDraw = mp.solutions.drawing_utils
+
+cap = cv2.VideoCapture('./videos/3.mp4')
+
+pTime = 0
+while True:
+    success , img = cap.read()
+    imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    results = pose.process(imgRGB)
+    print(results.pose_landmarks)
+
+    if results.pose_landmarks:
+        mpDraw.draw_landmarks(img,results.pose_landmarks,mpPose.POSE_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+    cv2.putText(img,str(int(fps)),(70,50),cv2.FONT_HERSHEY_PLAIN,3,(255,0,0),3)
+    cv2.imshow("Image", img)
+
+    cv2.waitKey(1)
